ea2111c3272b8661f8e004393c6c4c9710f5d40e
sofia qiriazi <sofia.qiriazi@gmail.com>
2018-08-13 14:14:38 +0200

    remove conflicts

--- server.R
+++ server.R
@@ -15673,12 +15673,7 @@ shinyServer(function(input, output, session) {
 
     region <- input$region
     fmonths_start <- which(current.long$Date == monthStart(as.Date("2017-02-01")))
-<<<<<<< HEAD
     fmonths_end <- which(current.long$Date == monthStart(as.Date("2018-08-01")))
-=======
-    fmonths_end <- which(current.long$Date == monthStart(as.Date("2018-05-01")))
->>>>>>> 9059e8e71921c0c09922df1b50a81e2a977d68de
-    # prepare columns for the merged graph
 
 
     if(region == "Bay"){
@@ -15860,12 +15855,7 @@ output$graph1 <- renderPlotly({
 
    region <- input$camp
    fmonths_start <- which(current.long$Date == monthStart(as.Date("2017-02-01")))
-<<<<<<< HEAD
    fmonths_end <- which(current.long$Date == monthStart(as.Date("2018-08-01")))
-=======
-   fmonths_end <- which(current.long$Date == monthStart(as.Date("2018-05-01")))
->>>>>>> 9059e8e71921c0c09922df1b50a81e2a977d68de
-   # prepare columns for the merged graph
 
    fun_1 <- paste(predictions.long[start_point,region],"arrivals",sep="")
    fun_2 <- paste(predictions.long[start_point+1,region],"arrivals",sep="")
